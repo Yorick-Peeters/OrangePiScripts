cb39cd18b708bdecc60bbe275c6d32716df26de4
YorickPeeters <r0990414@student.thomasmore.be>
2024-02-28 15:10:50 +0100

    verder gewekt

---
+++ Hoofdstuk3/LDRSensor.py
@@ -0,0 +1,20 @@
+import time
+import wiringpi
+import sys
+#SETUP
+print ("Start")
+pinLed = 2
+pinSwitch = 1
+wiringpi.wiringPiSetup() 
+wiringpi.pinMode (pinLed, 1) 
+wiringpi.pinMode (pinSwitch, 0)
+#infinite loop stop using CTRL-C Ewhile True:
+while True:
+    if (wiringpi.digitalRead(pinSwitch) == 0):
+        print("dark")
+        time.sleep(0.3)
+        wiringpi.digitalWrite(pinLed, 1)
+    else:
+        print("light")
+        time.sleep(0.3)
+        wiringpi.digitalWrite(pinLed, 0)
