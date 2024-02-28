3d3da17ae4f75322b49da65fb5c434b24e597c9e
YorickPeeters <r0990414@student.thomasmore.be>
2024-02-28 14:44:04 +0100

    Verder gewerkt aan voorbeelden

--- Hoofdstuk3/ledtest.py
+++ Hoofdstuk3/ledtest.py
@@ -4,19 +4,25 @@ import sys
 
 def blink(_pin):
     wiringpi.digitalWrite(_pin, 1)    # Write 1 ( HIGH ) to pin
-    time.sleep(0.5)
+    time.sleep(0.01)
     wiringpi.digitalWrite(_pin, 0)    # Write 1 ( HIGH ) to pin
-    time.sleep(0.5)
+    time.sleep(0.01)
 
 #SETUP
 print("Start")
-pin = 2
-wiringpi.wiringPiSetup() 
-wiringpi.pinMode(pin, 1)            # Set pin to mode 1 ( OUTPUT )
+pin4 = 4
+pin8 = 8
+pin10 = 10
+pin12 = 12
+# Set pin to mode 1 ( OUTPUT )
 
 #MAIN
-for i in range(0,10):
-    blink(pin)
+gpio.init()
+gpio.setmode(gpio.BOARD)
+gpio.setup(pin4, gpio.OUT)
+gpio.setup(pin8, gpio.OUT)
+gpio.setup(pin10, gpio.OUT)
+gpio.setup(pin12, gpio.OUT)
 
 #cleanup
 print("Done")
